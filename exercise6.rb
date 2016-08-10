2877c06b5ecc6a08022762e69cb711dc76b18978
titli1981 <madhumishra084@gmailcom>
2016-08-10 18:58:18 -0400

    Checked if bananas in the list

--- exercise6.rb
+++ exercise6.rb
@@ -8,5 +8,12 @@ grocery_list.each do |n|
   puts "* #{n}"
 end
 
+ grocery_list.include? "banana"
+ if true
+   puts "I need to pick up bananas today."
+else
+  puts "I do not need to pick up bananas today."
+end
+
 puts "There are #{grocery_list.length} items in the list"
 puts "The second item is #{grocery_list[1]}"
