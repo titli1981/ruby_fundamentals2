0d7f11cd46f880a4303d24716b1fcea8963b7f20
titli1981 <madhumishra084@gmailcom>
2016-08-10 16:31:30 -0400

    Made a list of grocery items

--- exercise5.rb
+++ exercise5.rb
@@ -3,8 +3,8 @@ puts "Type a number that you want to convert into Celcius:"
 temp = gets.chomp.to_i
 
 def temp_celcius(f)
-c = (f - 32) * 0.5555555556
+c = (f - 32) * (5.0/9) #0.5555555556
 
-puts "#{f} degree Farenheit is converted into #{c} degree Celcius"
+puts "#{f} °F is converted into #{c} °C "
 end
 temp_celcius(temp)
---
+++ exercise6.rb
@@ -0,0 +1,4 @@
+grocery_list = ["carrots", "bread", "apples", "salmon"]
+grocery_list.each do |item|
+  puts "* #{item}"
+end
